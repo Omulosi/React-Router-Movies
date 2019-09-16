3361c2c6275d51501c57292002aa1886d90cbb7c
Omulosi <mulongojohnpaul@gmail.com>
2019-09-16 23:03:37 +0300

    Finish stretch exercises

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieCard from './Movies/MovieCard';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +16,10 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component={MovieList}/>
+        <Route path="/movies/:id" component={props => <Movie {...props} addToSavedList={addToSavedList}/>} />
+      </div>
     </div>
   );
 };
